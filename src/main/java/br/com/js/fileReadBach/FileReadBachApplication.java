682b3b64b0c5ca86bf6f183eb45f29edd2ce4185
jclaudior <joaoclaudiodiva@gmail.com>
2022-07-05 20:40:33 -0300

    primeiro commit

---
+++ src/main/java/br/com/js/fileReadBach/FileReadBachApplication.java
@@ -0,0 +1,13 @@
+package br.com.js.fileReadBach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileReadBachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileReadBachApplication.class, args);
+	}
+
+}
